4b3fbb3e836af8d1691a12f88e6622174a9d16f3
lmanfredi <lmanfredi@minsait.com>
2022-06-25 18:11:14 +0200

    pdev: add breakpoint mixins

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,24 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  head: {
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'google', content: 'notranslate' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap' }
+    ]
+  },
 
+  css: [
+    'reset-css',
+    '~/assets/scss/main.scss'
+  ],
+
+  typescript: {
+    strict: true
+  }
 })
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+div.first-test
+  h1 hola
+  p {{ title }} 
+  //- img(src="~/assets/images/client_logo-circle.svg")
+  button(@click="addTitle('Lara')") press button
+</template>
+
+<script setup lang="ts">
+const title = ref('')
+
+const addTitle = (name: string) => {
+  title.value = name
+}
+</script>
+
+
+<style lang="scss" scoped>
+.first-test {
+  h1 {
+    font-size: 32px;
+  }
+}
+</style>
